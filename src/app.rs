b1a02b1ace10ff68b6bcbaf2098f72ab7b759670
Bèr Kessels <ber@berk.es>
2023-07-03 17:08:19 +0200

    feat: add uncontrolled form, that updates on submit

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,5 @@
-use leptos::*;
+use leptos::{ev::SubmitEvent, *};
+use leptos::html::Input;
 
 use crate::progress_bar::ProgressBar;
 
@@ -6,6 +7,18 @@ use crate::progress_bar::ProgressBar;
 pub fn App(cx: Scope) -> impl IntoView {
     // create a list of N signals
     let counters = (1..=40).map(|idx| create_signal(cx, idx));
+
+    // create a signal for the name
+    let (name, set_name) = create_signal(cx, "Bob".to_string());
+    let input_element: NodeRef<Input> = create_node_ref(cx);
+
+    let on_submit = move |ev: SubmitEvent| {
+        ev.prevent_default();
+
+        let value = input_element().expect("<input> to exists").value();
+
+        set_name(value);
+    };
 
     // each item manages a reactive view
     // but the list itself will never change
@@ -33,6 +46,15 @@ pub fn App(cx: Scope) -> impl IntoView {
         .collect_view(cx);
 
     view! { cx,
+        <h1>"Counter Buttons"</h1>
+        <form on:submit=on_submit>
+            <input type="text" value=name node_ref=input_element />
+            <input type="submit" value="Change Name" />
+        </form>
+
+        <pre>{name}</pre>
+
+        <hr/>
         <ul>{counter_buttons}</ul>
     }
 }
